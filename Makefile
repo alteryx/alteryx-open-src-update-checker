b7b2def34d13f094588dc6a404e4ee9e5cf30b99
Gaurav Sheni <gvsheni@gmail.com>
2019-05-14 17:30:58 -0400

    initial repo

---
+++ Makefile
@@ -0,0 +1,29 @@
+.PHONY: clean
+clean:
+	find . -name '*.pyo' -delete
+	find . -name '*.pyc' -delete
+	find . -name __pycache__ -delete
+	find . -name '*~' -delete
+
+.PHONY: lint
+lint:
+	flake8 featuretools_update_client && isort --check-only --recursive featuretools_update_client
+
+.PHONY: lint-fix
+lint-fix:
+	autopep8 --in-place --recursive --max-line-length=100 --exclude="*/migrations/*" --select="E225,E303,E302,E203,E128,E231,E251,E271,E127,E126,E301,W291,W293,E226,E306,E221" featuretools_update_client
+	isort --recursive featuretools_update_client
+
+.PHONY: test
+test: lint
+	pytest featuretools_update_client/tests
+
+.PHONY: testcoverage
+testcoverage: lint
+	pytest featuretools_update_client/tests --cov=featuretools_update_client
+
+.PHONY: installdeps
+installdeps:
+	pip install --upgrade pip
+	pip install -e .
+	pip install -r dev-requirements.txt
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='featuretools_update_client',
+    version='0.0.1',
+    packages=find_packages(),
+    author='Feature Labs, Inc.',
+    author_email='support@featurelabs.com',
+    classifiers=[
+         'Development Status :: 3 - Alpha',
+         'Intended Audience :: Developers',
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+         'Programming Language :: Python :: 3.5',
+         'Programming Language :: Python :: 3.6',
+         'Programming Language :: Python :: 3.7'
+    ],
+    install_requires=open('requirements.txt').readlines(),
+    setup_requires=open('setup-requirements.txt').readlines(),
+    python_requires='>=2.7, <4',
+    test_suite='featuretools_update_client/tests',
+    tests_require=open('test-requirements.txt').readlines(),
+    include_package_data=True,
+    long_description_content_type='text/markdown'
+)
