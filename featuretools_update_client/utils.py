a7e71fa510a9e65c544441572e22a40a9bd313ee
thehomebrewnerd <4307001+thehomebrewnerd@users.noreply.github.com>
2019-05-16 14:19:06 -0500

    remove rogue print statement

--- featuretools_update_client/utils.py
+++ featuretools_update_client/utils.py
@@ -6,8 +6,7 @@ from .settings import BASE_URL
 
 def get_response_json(version=ft.__version__):
     try:
-        response = requests.get(BASE_URL + '?version=' + version)
-        response = response.json()
+        response = requests.get(BASE_URL + '?version=' + version).json()
     except Exception:
         return None
 
